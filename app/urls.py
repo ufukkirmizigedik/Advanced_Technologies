af9cae61218d1fae14bea36a16723cf328732218
Ufuk <ufukkirmizigedik1984@gmail.com>
2024-08-21 17:05:37 +0300

    added russian language

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, Optical_glass, Optical_glass_processing_equipment, Optical_Instruments, about, contact, contact_form, contact_success
+from .views import index, Optical_glass, Optical_glass_processing_equipment, Optical_Instruments, about, contact
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,6 +8,4 @@ urlpatterns = [
     path('Optical_Instruments/', Optical_Instruments, name='Optical_Instruments'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-    path('contact_form/', contact_form, name='contact_form'),  # Corrected URL for contact_form
-    path('contact-success/', contact_success, name='contact_success'),  # Use contact_success view
 ]
